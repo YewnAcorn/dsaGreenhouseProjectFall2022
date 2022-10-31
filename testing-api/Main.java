7d551e2039f4e6ef3dea32ccc084fd6c5aaa6325
YewnAcorn <99105029+YewnAcorn@users.noreply.github.com>
2022-10-30 21:29:03 -0700

    method2 only works on java 11 or higher

--- testing-api/Main.java
+++ testing-api/Main.java
@@ -1,64 +1,20 @@
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+//this is method2 for getting API data in java
+//supposedly this one is better but only works on Java => 11
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
 
 public class Main
 {
-	private static HttpURLConnection connection;
-
 	public static void main(String args[])
 	{
-		//method 1: java.net.HttpURLConnection
-		BufferedReader reader;
-		String line;
-		StringBuffer responseContent = new StringBuffer();
-	try{
-		
-
-		URL url = new URL("https://api.openweathermap.org/data/2.5/weather?lat=47.10&lon=-123.05&appid=2509420cdb43a79b9f4fe083772cfe0c");
-		connection = (HttpURLConnection) url.openConnection();
-
-		//request set up
-		connection.setRequestMethod("GET");
-		connection.setConnectTimeout(5000);
-		connection.setReadTimeout(5000);
-
-		int status = connection.getResponseCode();
-		//System.out.println(status);
-		
-		if (status > 299)
-		{
-			reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-			while((line = reader.readLine()) != null)
-			{
-				responseContent.append(line);
-			}
-			reader.close();
-		} else
-		{
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			while((line = reader.readLine()) != null)
-			{
-				responseContent.append(line);
-			}
-			reader.close();
-		}
-		System.out.println(responseContent.toString());
-
-	} catch (MalformedURLException e) {
-			
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-				
-	} 
-	finally 
-	{
-		connection.disconnect();
-	}
-
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.openweathermap.org/data/2.5/weather?lat=47.10&lon=-123.05&appid=2509420cdb43a79b9f4fe083772cfe0c")).build();
+		client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+			.thenApply(HttpResponse::body)
+			.thenAccept(System.out::println)
+			.join();
 	}
 }
+
