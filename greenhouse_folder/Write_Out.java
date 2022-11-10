3699309d13c525012d5975fda36a804f4d414c6b
Brianna (she/her) <114614494+brianna-m@users.noreply.github.com>
2022-11-10 15:55:44 -0800

    Working with example of using it (commented)

--- greenhouse_folder/Write_Out.java
+++ greenhouse_folder/Write_Out.java
@@ -22,4 +22,10 @@ public class Write_Out {
         e.printStackTrace();
       }
     }
+//    Example of the formate for writing out to file.
+//     public static void main(String[] args){
+//       // Reverse_Lines_Template myInstance = new Reverse_Lines_Template();
+//       Write_Out.addLineToFile("date_time,\t temp,\t time_watered\n");
+//       Write_Out.addLineToFile("2022-11-10-15-51,\t 280,\t 40\n");
+    }
 }
