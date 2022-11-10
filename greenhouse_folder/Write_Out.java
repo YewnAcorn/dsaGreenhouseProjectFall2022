a96778303b741e6c53449ad4c8a20bd3694d1072
Brianna (she/her) <114614494+brianna-m@users.noreply.github.com>
2022-11-10 15:45:31 -0800

    Working. Static so we can use w/o instantiating.

--- greenhouse_folder/Write_Out.java
+++ greenhouse_folder/Write_Out.java
@@ -1,19 +1,16 @@
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-public class Write_Out {    
-  Write_Out(){
-
-    // Initializing file Path with some conditions
-    Path outputFilePath = Paths.get(".", "action_log.txt");
-    }
+public class Write_Out {
+  static Path outputFilePath;
   
-    public void addLineToFile(String dataLine){
+    public static void addLineToFile(String dataLine){
+
+      // Initializing file Path with some conditions
+      outputFilePath = Paths.get(".", "action_log.txt");
       try {
         Files.writeString(outputFilePath, dataLine + "\n", StandardOpenOption.APPEND);
         System.out.println(dataLine);
