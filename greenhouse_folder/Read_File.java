b1c76fed65e665aeca864fc11858ccc2d7403d93
Brianna (she/her) <114614494+brianna-m@users.noreply.github.com>
2022-11-09 20:55:58 -0800

    Added comments - see more
    Do we want to write out exceptions to the file when they happen?

--- greenhouse_folder/Read_File.java
+++ greenhouse_folder/Read_File.java
@@ -11,7 +11,11 @@ public class Read_File {
   // Initializing file Path with some conditions
   Path inputFilePath = Paths.get(".", "weather_data.txt");
   Charset charset = StandardCharsets.UTF_8;
-  List<String> lines;  
+  
+  // Create array of Strings for storing the data we read in
+  List<String> lines;
+  
+    // Try to read in the lines
     try {
       // read content from file
       lines = Files.readAllLines(inputFilePath, charset);
@@ -19,6 +23,10 @@ public class Read_File {
         System.out.println(line);
       }
     }
+
+    // Catch possible errors so it doesn't crash the program
+    // Might want to decide to take further action here
+//* // Maybe want to write the exception to the file?
     catch (IOException e) {
       // Failsafe in case writing to file fails
       System.out.println("Exception caught.");
