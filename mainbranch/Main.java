c0cc22686d18b6536d2880af604ee499cab22103
Brianna (she/her) <114614494+brianna-m@users.noreply.github.com>
2022-12-05 13:39:40 -0800

    Create Main.java

---
+++ mainbranch/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args){
+       
+    // cron is going to call main
+    
+    // get the weather info
+    
+    // main function in prototype is currently pulling weather api data & printint to terminal
+    // cronscript is saiving that output to output.json
+    // we can add a line there to also save the data to weather_log.txt
+    
+    
+
+}
