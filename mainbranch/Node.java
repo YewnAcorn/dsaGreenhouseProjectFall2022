f63d0c71cd6d623cebaf6757bfa896c9454c2c5f
Brianna (she/her) <114614494+brianna-m@users.noreply.github.com>
2022-12-05 12:01:09 -0800

    Update Node.java

--- mainbranch/Node.java
+++ mainbranch/Node.java
@@ -1,1 +1,9 @@
-
+public class Node {
+    public String date;
+    public String status;
+    public Node next;
+    public Node prev;
+    public boolean water;
+    Node(String x, String y, boolean z) { status = x; date = y; water = z; }
+    
+}
