a7efd5ad5d4e17ef1cfab60e3af6bdf07ff4af79
Brianna (she/her) <114614494+brianna-m@users.noreply.github.com>
2022-12-09 13:42:10 -0800

    Update cronscript.sh

--- mainbranch/cronscript.sh
+++ mainbranch/cronscript.sh
@@ -5,7 +5,7 @@ cd /home/pi/Gardenbot/dsaGreenhouseProjectFall2022/testing-api/
 
 # run our Java program, which prints out weather info to weather_data.txt
 # shotcoaller decides whether to water & writes date & t/f (did we water?) to activity_log.txt
-java Main > output.json
+java Main
 
 # checks the activity log to see if it's supposed to turn on the water. Sends power to the pins
 # on the Pi to water if we want to water today
